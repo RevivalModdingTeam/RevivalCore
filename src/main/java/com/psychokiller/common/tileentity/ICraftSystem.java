f580d68f8baac116379d5ea84b7e39ec9774d7bb
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-04-03 20:20:58 +0200

    crafting base - alpha stage

--- src/main/java/com/psychokiller/common/tileentity/ICraftSystem.java
+++ src/main/java/com/psychokiller/common/tileentity/ICraftSystem.java
@@ -2,8 +2,11 @@ package com.psychokiller.common.tileentity;
 
 import java.util.Set;
 
+import com.psychokiller.recipes.SHIngredient;
 import com.psychokiller.recipes.SHRecipe;
 import com.psychokiller.revivalcore.RevivalCore;
+
+import net.minecraft.item.ItemStack;
 
 /**
  * @author Toma1O6
@@ -16,9 +19,43 @@ public interface ICraftSystem<R extends SHRecipe>
 		if(!te.getStackInSlot(getOutput()).isEmpty())
 			return;
 		
+		boolean running = true;
+		SHRecipe recipe = null;
+		
 		for(SHRecipe rec : this.getRegistry())
 		{
+			for(SHIngredient i : rec.getIngredients())
+			{
+				if(!ItemStack.areItemsEqual(te.getStackInSlot(i.index), i.ingredient))
+				{
+					running = false;
+					break;
+				}
+			}
 			
+			if(running)
+			{
+				recipe = rec;
+				break;
+			}
+		}
+		
+		// all ingredients were valid (ingnoring slots which weren't specified by the recipe)
+		if(running && recipe != null)
+		{
+			te.setInventorySlotContents(this.getOutput(), recipe.getResult());
+			this.consumeIngredients(recipe, te);
+		}
+	}
+	
+	default void consumeIngredients(SHRecipe recipe, TileEntitySH te)
+	{
+		for(int i = 0; i < recipe.getIngredients().length; i++)
+		{
+			ItemStack ingredient = recipe.getIngredients()[i].ingredient;
+			int index = recipe.getIngredients()[i].index;
+			ItemStack teStack = te.getStackInSlot(index);
+			te.setInventorySlotContents(index, new ItemStack(teStack.getItem(), teStack.getCount() - ingredient.getCount()));
 		}
 	}
 	
