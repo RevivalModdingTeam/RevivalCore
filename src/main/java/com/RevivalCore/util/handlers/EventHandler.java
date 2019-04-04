a517dd0887348a1045d7cdb8787602eac814c85e
Josia50 <elwyn.vdb@gmail.com>
2019-04-04 18:16:43 +0200

    Slowmotion Temp

--- src/main/java/com/RevivalCore/util/handlers/EventHandler.java
+++ src/main/java/com/RevivalCore/util/handlers/EventHandler.java
@@ -1,10 +1,23 @@
 package com.RevivalCore.util.handlers;
 
+import com.RevivalCore.util.helper.powers.SlowMotionAPI;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler
 {
  // TODO Handle Stuff
+ public static boolean doAllowed = true;
+
+    @SubscribeEvent
+    public static void EntityLivingEvent(LivingEvent.LivingUpdateEvent e) {
+        if(doAllowed) {
+            EntityPlayer player = (EntityPlayer) e.getEntity();
+             SlowMotionAPI.SlowProjectiles(player, 0.3);
+        }
+    }
 }
 
