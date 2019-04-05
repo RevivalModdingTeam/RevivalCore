d9ec6f14bab464c608594fb47d5dd3a2c536c57d
Josia50 <elwyn.vdb@gmail.com>
2019-04-05 20:44:08 +0200

    Item Locked

---
+++ src/main/java/com/revivalcore/util/RCTeam.java
@@ -0,0 +1,8 @@
+package com.revivalcore.util;
+
+public class RCTeam {
+
+    public static final String JOSIA50 = "a0b89882-8509-42b2-921b-14facddfb5dc";
+    public static final String PSYCHOKILLER = "";
+    public static final String TOMA = "5915a7bf-7481-4914-b901-8ef3f4555797";
+}
