2c7ca65edce1ff150b9a71e6aa4756d8b263ff15
Josia50 <elwyn.vdb@gmail.com>
2019-04-05 17:06:22 +0200

    Speed & Slowmo Base

--- src/main/java/com/revivalcore/util/helper/powers/SlowMotionAPI.java
+++ src/main/java/com/revivalcore/util/helper/powers/SlowMotionAPI.java
@@ -12,11 +12,11 @@ public class SlowMotionAPI {
      * on 4/04/2019.
      */
 
-    public static void SlowProjectiles(EntityPlayer player, double speed) {
+    public static void SlowProjectiles(EntityPlayer player, int range ,double speed) {
         double s = speed; // Default speed is 0.3 //
          int v = 0;
 
-        for (Entity e : player.world.getEntitiesWithinAABB(Entity.class, player.getEntityBoundingBox().grow(5, 5, 5))) {
+        for (Entity e : player.world.getEntitiesWithinAABB(Entity.class, player.getEntityBoundingBox().grow(range, range, range))) {
             if (e instanceof IProjectile) {
                 e.getEntityData().setInteger("edited", 0);
                 if (e.getEntityData().getInteger("edited") == 0) {
@@ -29,17 +29,23 @@ public class SlowMotionAPI {
                         e.setVelocity(v, v, s);
                     }
 
-                    if (e.getHorizontalFacing() == EnumFacing.EAST) {
+                    if (e.getHorizontalFacing() == EnumFacing.EAST) { // TODO FIX EAST
                         e.setVelocity(s, v, v);
-                    }
+                    }                                                // StackTrace = Checks for North & South rotation only could be fixed with angles and being limited angle.
 
-                    if (e.getHorizontalFacing() == EnumFacing.WEST) {
+                    if (e.getHorizontalFacing() == EnumFacing.WEST) { // TODO FIX WEST
                         e.setVelocity(s, v, v);
                     }
                     System.out.println(e.getHorizontalFacing());
-                    e.getEntityData().setInteger("edited", 1);
+                    e.getEntityData().setInteger("edited", 1); // TODO use NBT Helper
                 }
             }
+        }
+    }
+
+    public static void SlowOtherPlayers(EntityPlayer player, int range) {
+        for(EntityPlayer player1 : player.world.getEntitiesWithinAABB(EntityPlayer.class, player.getEntityBoundingBox().grow(range, range, range))) {
+            SpeedAPI.setSpeedToCap(player1, 1.0F);
         }
     }
 }
--- src/main/java/com/revivalcore/util/helper/powers/SpeedAPI.java
+++ src/main/java/com/revivalcore/util/helper/powers/SpeedAPI.java
@@ -1,5 +1,9 @@
 package com.revivalcore.util.helper.powers;
 
+import com.revivalcore.capabilities.CapSpeedstersStorage;
+import com.revivalcore.capabilities.CapabilitySpeedster;
+import net.minecraft.entity.player.EntityPlayer;
+
 /**
  * Created by Josia50
  * on 5/04/2019.
@@ -7,4 +11,20 @@ package com.revivalcore.util.helper.powers;
 
 public class SpeedAPI {
 
+    public static void setSpeed(EntityPlayer player, float level) {
+        player.setAIMoveSpeed(level);
+    }
+
+    public static void setSpeedFromCap(EntityPlayer player) {
+        CapabilitySpeedster cap = player.getCapability(CapSpeedstersStorage.CAP, null);
+        // setSpeed(player, cap.getSpeedLevel); // TODO getSpeedLevel initialize
+        cap.sync();
+    }
+
+    public static void setSpeedToCap(EntityPlayer player, float level) {
+        CapabilitySpeedster cap = player.getCapability(CapSpeedstersStorage.CAP, null);
+        // cap.setSpeedLevel(float);
+        setSpeed(player, level);
+        cap.sync();
+    }
 }
