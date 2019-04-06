88562aee100d5241e11e69aa318db9e6c758f6f4
Josia50 <elwyn.vdb@gmail.com>
2019-04-06 23:21:39 +0200

    0.0.2

--- src/main/java/com/revivalcore/core/RevivalCore.java
+++ src/main/java/com/revivalcore/core/RevivalCore.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.Logger;
 public class RevivalCore {
     public static final String MODID = "revivalcore";
     public static final String NAME = "Revival Core";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.2";
 
     @Instance
     public static RevivalCore instance;
