184f8bf3d4dbd23f710a7f6f6fecfee18bcc6507
Josia50 <elwyn.vdb@gmail.com>
2019-04-08 20:18:51 +0200

    PlayerHelper

--- src/main/java/com/revivalcore/util/helper/PlayerHelper.java
+++ src/main/java/com/revivalcore/util/helper/PlayerHelper.java
@@ -10,4 +10,20 @@ public class PlayerHelper {
             player.sendStatusMessage(new TextComponentTranslation(mes), statusbar);
         }
     }
+
+    public static void setboolean(EntityPlayer player, String name, boolean bool) {
+        player.getEntityData().setBoolean(name, bool);
+    }
+
+    public static void setint(EntityPlayer player, String name, int in) {
+        player.getEntityData().setInteger(name, in);
+    }
+
+    public static boolean getboolean(EntityPlayer player, String name) {
+        return player.getEntityData().getBoolean(name);
+    }
+
+    public static int getint(EntityPlayer player, String name) {
+        return player.getEntityData().getInteger(name);
+    }
 }
