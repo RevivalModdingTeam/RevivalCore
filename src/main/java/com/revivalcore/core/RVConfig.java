7fac33c0002d0b37485fbbcd82e6df6bc7d6b979
Josia50 <elwyn.vdb@gmail.com>
2019-04-09 22:43:04 +0200

    First Part Update Checker

--- src/main/java/com/revivalcore/core/RVConfig.java
+++ src/main/java/com/revivalcore/core/RVConfig.java
@@ -1,15 +1,23 @@
 package com.revivalcore.core;
 
-import com.revivalcore.core.RevivalCore;
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Config.Type;
+import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = RevivalCore.MODID, name = RevivalCore.NAME + " Config")
 public class RVConfig {
+
+    public static final RCore revivalCore = new RCore();
+
+
+    public static class RCore {
+        @Config.LangKey("config.shr.updatechecker")
+        @Config.Comment("Toggle the update checker")
+        public boolean updatechecker = true;
+    }
 
     @Mod.EventBusSubscriber
     public static class Event {
--- src/main/java/com/revivalcore/util/handlers/EventHandler.java
+++ src/main/java/com/revivalcore/util/handlers/EventHandler.java
@@ -1,10 +1,32 @@
 package com.revivalcore.util.handlers;
 
+import com.revivalcore.core.RVConfig;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler
 {
- // TODO Handle Stuff
+    @SubscribeEvent
+    public static void PlayerJoinWorld(PlayerEvent.PlayerLoggedInEvent playerEvent) {
+        EntityPlayer player = playerEvent.player;
+        if (!player.world.isRemote && RVConfig.revivalCore.updatechecker) {
+            ForgeVersion.CheckResult version = ForgeVersion.getResult(Loader.instance().activeModContainer());
+            if (version.status.equals(ForgeVersion.Status.OUTDATED)) {
+                TextComponentString msg = new TextComponentString(TextFormatting.BLUE + "[RevivalCore] : New Update Available!");
+                msg.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.curseforge.com/projects/revivalcore"));
+                msg.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("Open Website")));
+                player.sendMessage(msg);
+            }
+        }
+    }
 }
 
