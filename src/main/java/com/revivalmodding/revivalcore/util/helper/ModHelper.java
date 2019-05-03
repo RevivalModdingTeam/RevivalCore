afd589a51d552df6ae9f2c4ce1530d9d3a725599
Josia50 <elwyn.vdb@gmail.com>
2019-05-03 22:38:16 +0200

    Reverse

--- src/main/java/com/revivalmodding/revivalcore/util/helper/ModHelper.java
+++ src/main/java/com/revivalmodding/revivalcore/util/helper/ModHelper.java
@@ -1,6 +1,5 @@
 package com.revivalmodding.revivalcore.util.helper;
 
-import com.revivalmodding.revivalcore.RevivalCore;
 import net.minecraft.launchwrapper.Launch;
 
 public class ModHelper {
@@ -10,10 +9,8 @@ public class ModHelper {
     }
 
     public static void startupChecks() {
-        if(!RevivalCore.run) {
-            if (!getIsDev()) {
-                throw new IllegalStateException("You're not allowed to run this!");
-            }
+        if (!getIsDev()) {
+
         }
     }
 }
