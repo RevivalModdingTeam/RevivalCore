0762050a1c0460da7dffaf85369f9b829368feb7
Josia50 <elwyn.vdb@gmail.com>
2019-05-08 21:32:24 +0200

    Translucent

--- src/main/java/com/revivalmodding/revivalcore/core/common/blocks/BlockSuitMaker.java
+++ src/main/java/com/revivalmodding/revivalcore/core/common/blocks/BlockSuitMaker.java
@@ -17,10 +17,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -116,6 +113,11 @@ public class BlockSuitMaker extends Block implements ITileEntityProvider, IHaveI
     @Override
     public EnumBlockRenderType getRenderType(IBlockState state) {
         return EnumBlockRenderType.INVISIBLE;
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.TRANSLUCENT;
     }
 
     @Override
