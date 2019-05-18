5cf9d66db2daee194d92f4a4bf836973e62f18b6
Josia50 <elwyn.vdb@gmail.com>
2019-05-18 13:12:47 +0200

    1.6

--- src/main/java/com/revivalmodding/revivalcore/RevivalCore.java
+++ src/main/java/com/revivalmodding/revivalcore/RevivalCore.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 public class RevivalCore {
     public static final String MODID = "revivalcore";
     public static final String NAME = "Revival Core";
-    public static final String VERSION = "0.1.4";
+    public static final String VERSION = "0.1.6";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/RevivalModdingTeam/RevivalModding-ModBugs/master/updatecore.json";
 
 
