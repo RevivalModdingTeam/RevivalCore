126f25f1518f6b17b4b51c34ac7f22b5b3ac5d2a
Josia50 <elwyn.vdb@gmail.com>
2019-05-19 10:42:28 +0200

    Itemlocked

--- src/main/java/com/revivalmodding/revivalcore/core/common/items/ItemLocked.java
+++ src/main/java/com/revivalmodding/revivalcore/core/common/items/ItemLocked.java
@@ -1,27 +1,27 @@
 package com.revivalmodding.revivalcore.core.common.items;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.revivalmodding.revivalcore.util.RCTeam;
+import com.revivalmodding.revivalcore.util.helper.ModHelper;
 import com.revivalmodding.revivalcore.util.helper.PlayerHelper;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemLocked extends Item {
 
     public static final List<String> UUIDS = new ArrayList<String>();
 
     public ItemLocked() {
-        this.UUIDS.add(RCTeam.JOSIA50);
-        this.UUIDS.add(RCTeam.TOMA);
-        this.UUIDS.add(RCTeam.TUD);
+        UUIDS.addAll(ModHelper.teamMembers);
     }
 
+    public static void addAllowedUser(String uuid) {
+        UUIDS.add(uuid);
+    }
 
     @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
@@ -29,13 +29,21 @@ public class ItemLocked extends Item {
         if (entityIn instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) entityIn;
             if (isSelected) {
-                for(String s : UUIDS) {
-                    if(!(player.getUniqueID().toString().equals(s))) {
-                        stack.setCount(0);
-                        PlayerHelper.sendMessage(player, "[RevivalCore] You can't use this item!", false);
-                    }
+                if (!isUserAllowed(player)) {
+                    stack.setCount(0);
+                    PlayerHelper.sendMessage(player, "[RevivalCore] You can't use this item!", true);
                 }
             }
         }
+
+    }
+
+    public static boolean isUserAllowed(EntityPlayer player) {
+        for (String s : UUIDS) {
+            if (player.getUniqueID().toString().equals(s)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
