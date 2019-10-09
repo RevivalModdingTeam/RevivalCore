0844119e45a170ac673e31667709499cea5ac647
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-10-09 20:47:33 +0200

    Update LayerSuitBase.java

--- src/main/java/com/revivalmodding/revivalcore/core/client/render/LayerSuitBase.java
+++ src/main/java/com/revivalmodding/revivalcore/core/client/render/LayerSuitBase.java
@@ -1,5 +1,6 @@
 package com.revivalmodding.revivalcore.core.client.render;
 
+import com.revivalmodding.revivalcore.core.common.suits.AbstractSuit;
 import com.revivalmodding.revivalcore.core.common.suits.ItemSuit;
 import com.revivalmodding.revivalcore.util.handlers.client.SuitRenderHandler;
 import net.minecraft.client.model.ModelBiped;
@@ -9,14 +10,23 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nullable;
+
 public class LayerSuitBase<E extends EntityLivingBase> implements LayerRenderer<E> {
 
     private final RenderLivingBase<?> renderer;
+    @Nullable
+    private final AbstractSuit suit;
     public ModelBiped mainModel;
 
     public LayerSuitBase(RenderLivingBase<?> renderer) {
+        this(renderer, null);
+    }
+
+    public LayerSuitBase(RenderLivingBase<?> renderer, AbstractSuit suit) {
         this.renderer = renderer;
         this.initModel();
+        this.suit = suit;
     }
 
     public void initModel() {
@@ -36,6 +46,16 @@ public class LayerSuitBase<E extends EntityLivingBase> implements LayerRenderer<
     }
 
     private void renderLayerPart(E entity, float limbSwing, float limbSwingAmount, float partial, float age, float headYaw, float headPitch, float scale, EntityEquipmentSlot slot) {
+        if(suit != null) {
+            // For entities with defined suits
+            mainModel.setModelAttributes(this.renderer.getMainModel());
+            mainModel.setLivingAnimations(entity, limbSwing, limbSwingAmount, partial);
+            this.setVisibleParts(slot);
+            this.renderer.bindTexture(this.suit.getHelmet().get3DTexture());
+            mainModel.render(entity, limbSwing, limbSwingAmount, age, headYaw, headPitch, scale);
+            return;
+        }
+        // For players
         ItemStack stack = entity.getItemStackFromSlot(slot);
         if (!stack.isEmpty() && stack.getItem() instanceof ItemSuit) {
             ItemSuit suit = (ItemSuit) stack.getItem();
