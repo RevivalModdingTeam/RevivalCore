70ea098ddf2105e6a51164e8ed5be1c164ef8d39
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2019-11-24 11:24:46 +0100

    made trail rendering a bit nicer

--- src/main/java/com/revivalmodding/revivalcore/core/client/trail/renderers/TrailRendererSimple.java
+++ src/main/java/com/revivalmodding/revivalcore/core/client/trail/renderers/TrailRendererSimple.java
@@ -16,23 +16,22 @@ public class TrailRendererSimple extends TrailRenderer {
     public void renderTrail(EntityPlayer player, Trail trailToRender, @Nullable TrailOptionalData trailData, float partialTick) {
         AbstractSuit suit = AbstractSuit.getSuit(player);
         int color = suit != null ? suit.getTrailRGB().getRGB() : trailToRender.getColor();
-        Vec3d lastRenderVec = null;
         double x = this.interpolate(player.posX, player.lastTickPosX, partialTick);
         double y = this.interpolate(player.posY, player.lastTickPosY, partialTick);
         double z = this.interpolate(player.posZ, player.lastTickPosZ, partialTick);
         Trail.TrailPos[] vertexData = trailToRender.getVertexData();
-        for(int p = 0; p < vertexData.length; p++) {
+        Vec3d lastRenderVec = new Vec3d(x, y, z);
+        for(int p = 1; p < vertexData.length; p++) {
             Trail.TrailPos trail = vertexData[p];
             if(trail == null) continue;
             int trailWidth = trailToRender.getWidth() - p;
             trailWidth = trailWidth <= 0 ? 1 : trailWidth;
             GlStateManager.glLineWidth(trailWidth);
             float a = trail.alpha;
-            // TODO fix: First trail is never rendered
+            float r = ((color >> 16) & 255) / 255.0F;
+            float g = ((color >>  8) & 255) / 255.0F;
+            float b = (color & 255) / 255.0F;
             if(lastRenderVec != null) {
-                float r = ((color >> 16) & 255) / 255.0F;
-                float g = ((color >>  8) & 255) / 255.0F;
-                float b = (color & 255) / 255.0F;
                 for(TrailPart part : TrailPart.values()) {
                     Vec3d partVec = part.offset().rotateYaw(-player.rotationYaw * 0.017453292F - ((float)Math.PI / 2.0F));
                     Vec3d renderVec = trail.add(partVec.x, partVec.y, partVec.z);
@@ -42,8 +41,6 @@ public class TrailRendererSimple extends TrailRenderer {
                         this.drawLine(renderVec, last, trailWidth, r, g, b, a, x, y, z);
                     }
                 }
-            } else {
-
             }
             lastRenderVec = trail;
         }
