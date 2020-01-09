bfb8fee2946dce0ff984bf1ca463224a71328c57
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2020-01-09 17:42:20 +0100

    updated version

--- src/main/java/com/revivalmodding/revivalcore/RevivalCore.java
+++ src/main/java/com/revivalmodding/revivalcore/RevivalCore.java
@@ -32,7 +32,7 @@ import java.util.Random;
 public class RevivalCore {
     public static final String MODID = "revivalcore";
     public static final String NAME = "Revival Core";
-    public static final String VERSION = "0.3.8";
+    public static final String VERSION = "0.3.9";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/RevivalModdingTeam/RevivalModding-ModBugs/master/update/updatecore.json";
     private static final Random random = new Random();
 
