d38147b94d82b90b455cfcb92fa01b9a2793814b
Chappie <53862142+ChappiePie@users.noreply.github.com>
2020-04-13 17:03:03 +0300

    Trail positions set

--- src/main/java/com/revivalmodding/revivalcore/core/abilities/AbilityConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.revivalmodding.revivalcore.core.abilities;
-
-import net.minecraft.entity.player.EntityPlayer;
-
-public interface AbilityConsumer {
-
-    void apply(Ability ability, EntityPlayer player);
-}
--- src/main/java/com/revivalmodding/revivalcore/core/client/trail/TrailPart.java
+++ src/main/java/com/revivalmodding/revivalcore/core/client/trail/TrailPart.java
@@ -3,13 +3,24 @@ package com.revivalmodding.revivalcore.core.client.trail;
 import net.minecraft.util.math.Vec3d;
 
 public enum TrailPart {
-
-    HEAD(-0.2, 1.7, 0),
     BODY(-0.2, 1, 0),
-    RIGHT_ARM(0.2, 1.2, 0.5),
-    LEFT_ARM(0.2, 1.2, -0.5),
-    RIGHT_LEG(-0.2, 0.4, 0.25),
-    LEFT_LEG(-0.2, 0.4, -0.25);
+
+    RIGHT_ARM(-0.2, 1.2, 0.25),
+    LEFT_ARM(-0.2, 1.2, -0.25),
+
+    RIGHT_LEG(-0.2, 0.4, 0.20),
+    LEFT_LEG(-0.2, 0.4, -0.20),
+
+    RIGHT(-0.2, 1.7, 0.15),
+    LEFT(-0.2, 1.7, -0.15),
+
+    HEAD(-0.2, 1.4, 0),
+
+    RIGHT_ARM_2(-0.2, 1.2, 0.15),
+    LEFT_ARM_2(-0.2, 1.2, -0.15),
+
+    RIGHT_LEG_2(-0.2, 0.9, 0.22),
+    LEFT_LEG_2(-0.2, 0.9, -0.22);
 
     private Vec3d offset;
 
